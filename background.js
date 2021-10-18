028c3955749d2858c026e361e79665c8fe70f6c5
Lavanya Yoganantham <y509418@softwareag.com>
2021-10-18 17:52:41 +0530

    Init

---
+++ background.js
@@ -0,0 +1,66 @@
+
+'use strict';
+
+ chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log('The color is green.');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+     chrome.declarativeContent.onPageChanged.addRules([{
+       conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'localhost'},
+       })
+      ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+   });
+  });
+  
+chrome.tabs.onUpdated.addListener(
+  function(tabId, changeInfo, tab) {
+    // read changeInfo data and do something with it
+    // like send the new url to contentscripts.js
+    if (changeInfo.url) {
+      chrome.tabs.sendMessage( tabId, {
+        message: 'tabsUpdated!',
+        url: changeInfo.url
+      })
+    }
+  }
+);
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+		
+    // Take screenshot
+    if (request.greeting == "take_screenshot"){
+      chrome.tabs.captureVisibleTab(null,{},function(dataUrl){
+        window.screenshot = dataUrl;
+        // console.log(window.screenshot);
+        sendResponse({farewell: window.screenshot});
+      });
+      return true;
+    }
+		
+    }
+);
+
+chrome.browserAction.onClicked.addListener(function(tab) { 
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		  chrome.tabs.executeScript({
+          file: 'contentScript.js'
+        });
+    });
+    // chrome.tabs.create({
+    //     url: chrome.extension.getURL('contentScript.js'),
+    //     active: false
+    // }, function(tab) {
+    //     // After the tab has been created, open a window to inject the tab
+    //     chrome.windows.create({
+    //         tabId: tab.id,
+    //         type: 'popup',
+    //         focused: true
+    //         // incognito, top, left, ...
+    //     });
+    // });
+});
